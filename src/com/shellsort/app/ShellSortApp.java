7c9eaf31be53104a2ccae3be258ba1b91e6f7612
devjosegomez <devjosegomez@gmail.com>
2019-10-19 21:05:16 -0500

    Shell sort implementation

--- src/com/shellsort/app/ShellSortApp.java
+++ src/com/shellsort/app/ShellSortApp.java
@@ -1,10 +1,43 @@
+//Example or Shell Sort
 package com.shellsort.app;
 
 public class ShellSortApp {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
 
-	}
+	    int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
+	    
+	    //Displaying Unsorted Array
+	    System.out.println("--- Shell sort ---");
+        System.out.println("Unsorted array:");
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.print("[" + intArray[i] + "]");
+        }
+	    
+        for (int gap = intArray.length / 2; gap > 0; gap /= 2) {
+
+
+            for (int i = gap; i < intArray.length; i++) {
+                int newElement = intArray[i];
+
+                int j = i;
+
+                while (j >= gap && intArray[j - gap] > newElement) {
+                    intArray[j] = intArray[j - gap];
+                    j -= gap;
+                }
+
+                intArray[j] = newElement;
+
+            }
+
+        }
+        
+	    //Displaying Sorted Array
+        System.out.println("\nSorted array:");
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.print("[" + intArray[i] + "]");
+        }
+    }
 
 }
